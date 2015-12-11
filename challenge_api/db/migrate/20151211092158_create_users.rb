a30354bc3bc638f7f4f80e22c33751f03fe8117c
sukeshni <sukeshni.kantrod@gmail.com>
2015-12-11 18:51:09 +0900

    Mod users table- add constraints

--- challenge_api/db/migrate/20151211092158_create_users.rb
+++ challenge_api/db/migrate/20151211092158_create_users.rb
@@ -1,14 +1,15 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :password
-      t.string :email
+      t.string :username, :null =>false, :limit =>100
+      t.string :password, :null =>false, :limit =>100
+      t.string :email, :null =>false, :limit =>100, :unique =>true
       t.date :birthday
-      t.string :company
-      t.string :location
+      t.string :company, :limit =>50
+      t.string :location, :limit =>50
 
       t.timestamps null: false
     end
+    add_index :users, :email, :unique => true
   end
 end
--- challenge_api/db/seeds.rb
+++ challenge_api/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# u = User.create(username: "user1", password: "password", email: "user1@test.com", birthday: "1991-04-17", company: "company1", location: "location1")
+u = User.create(username: "user2", password: "password", email: "user2@test.com", birthday: "1989-04-17", company: "company2", location: "location2")
+u = User.create(username: "user3", password: "password", email: "user3@test.com", birthday: "1995-04-17", company: "company3", location: "location1")
+u = User.create(username: "user4", password: "password", email: "user4@test.com", birthday: "1987-04-17", company: "company2", location: "location1")
+u = User.create(username: "user5", password: "password", email: "user5@test.com", birthday: "1999-04-17", company: "company2", location: "location2")
+u = User.create(username: "user6", password: "password", email: "user6@test.com", birthday: "1994-04-17", company: "company1", location: "location2")
