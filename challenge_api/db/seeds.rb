bd578e1cdd2eafa0fdc44fec6fff8deb94cd5de2
sukeshni <sukeshni.kantrod@gmail.com>
2015-12-15 15:26:29 +0900

    small fix - uncomment seed data

--- challenge_api/db/seeds.rb
+++ challenge_api/db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# u = User.create(username: "user1", password: "password", email: "user1@test.com", birthday: "1991-04-17", company: "company1", location: "location1")
+u = User.create(username: "user1", password: "password", email: "user1@test.com", birthday: "1991-04-17", company: "company1", location: "location1")
 u = User.create(username: "user2", password: "password", email: "user2@test.com", birthday: "1989-04-17", company: "company2", location: "location2")
 u = User.create(username: "user3", password: "password", email: "user3@test.com", birthday: "1995-04-17", company: "company3", location: "location1")
 u = User.create(username: "user4", password: "password", email: "user4@test.com", birthday: "1987-04-17", company: "company2", location: "location1")
